2448703411f474685c6a06cc340755aef96207e2
vuthisak <khyvuthisak@gmail.com>
2023-07-19 00:34:50 +0700

    Updated main UI

--- app/src/main/java/com/example/clickspeedtest/feature/selectmode/SelectModeActivity.kt
+++ app/src/main/java/com/example/clickspeedtest/feature/selectmode/SelectModeActivity.kt
@@ -1,11 +1,13 @@
 package com.example.clickspeedtest.feature.selectmode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.clickspeedtest.R
 import com.example.clickspeedtest.base.BaseActivity
 import com.example.clickspeedtest.databinding.ActivitySelecteModeBinding
+import com.example.clickspeedtest.feature.main.MainActivity
 import com.example.clickspeedtest.feature.selectmode.adapter.SelectModeAdapter
 import com.example.clickspeedtest.util.GeneralUtils
 import com.example.network.model.SelectMode
@@ -20,8 +22,11 @@ class SelectModeActivity : BaseActivity<ActivitySelecteModeBinding>(),
         setListener()
     }
 
-    override fun onItemClicked(selectMode: SelectMode) {
-        Toast.makeText(this, selectMode.text, Toast.LENGTH_SHORT).show()
+    override fun onItemClicked(selectMode: SelectMode) = gotoMainScreen()
+
+    private fun gotoMainScreen() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
     private fun setupContent() {
